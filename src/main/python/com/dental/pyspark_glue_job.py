cf5c4b508978dd662f8b86e6b5abcbc7c912a506
Amadou Ndiaye <ahmad.ndiayee@gmail.com>
2021-07-18 23:31:25 +0000

    add function get_nested_output

--- src/main/python/com/dental/pyspark_glue_job.py
+++ src/main/python/com/dental/pyspark_glue_job.py
@@ -1,12 +1,9 @@
 import os
-
-from pyspark.sql.functions import col, explode, lit, from_json, struct, collect_list
+from pyspark.sql.functions import explode, lit, collect_list, col, struct
 from pyspark.sql import SparkSession
 import configparser
 
-from pyspark.sql.types import StructType, ArrayType
-
-CONF_FILE = '/home/ahmad/IdeaProjects/pyspark_glue_jobs/src/main/resources/config.ini'
+CONF_FILE = '/Users/macbookpro/IdeaProjects/pyspark_glue_jobs/src/main/resources/config.ini'
 
 config = configparser.ConfigParser()
 config.read(os.path.expanduser("~/.aws/credentials"))
@@ -15,13 +12,12 @@ access_key = config.get("glue_jobs", "aws_secret_access_key")
 
 spark = SparkSession.builder \
     .appName("GLUE_JOBS") \
-    .master("local[*]") \
+    .master("local[1]") \
     .config("spark.sql.legacy.timeParserPolicy", "LEGACY") \
     .config("spark.hadoop.fs.s3a.access.key", access_id) \
     .config("spark.hadoop.fs.s3a.secret.key", access_key) \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
-
 
 config.read(CONF_FILE)
 input_location = config.get("paths", 'input')
@@ -43,7 +39,7 @@ file = explode_nested_array(file)
 
 
 def get_nested_columns(df):
-    df = df.select(lit(count).alias("count"), col("sequence"), col("activatedDate").alias("start_date"),
+    df = df.select(col("sequence"), col("activatedDate").alias("start_date"),
                    col("identifier"),
                    col("_individualNames_.attributes.FirstName").alias("given_name"),
                    col("_individualNames_.attributes.LastName").alias("other_given_names"),
@@ -62,7 +58,6 @@ def get_nested_columns(df):
 
 def process_data(df):
     df = df.select(
-        col("count"),
         col("sequence"),
         col("start_date"),
         col("identifier"),
@@ -85,31 +80,50 @@ def process_data(df):
                 col("start_date").alias("start_date")
             ).alias("status_histories")
         ).alias("entitlements"),
-        col("YearOfBirth"),
-        col("MonthOfBirth"),
-        col("DayOfBirth")
+        col("YearOfBirth").alias("year_of_birth"),
+        col("MonthOfBirth").alias("month_of_birth"),
+        col("DayOfBirth").alias("day_of_birth")
     ).groupBy(
-        col("count"),
         col("sequence"),
         col("start_date"),
         col("identifier"),
-        col("YearOfBirth"),
-        col("MonthOfBirth"),
-        col("DayOfBirth")
+        col("year_of_birth"),
+        col("month_of_birth"),
+        col("day_of_birth")
     ).agg(
         collect_list("names").alias("names"),
         collect_list("entitlements").alias("entitlements")
-    )
+    ).select(col("sequence"), col("start_date"), col("identifier"), col("names"), col("entitlements"),
+             col("year_of_birth"), col("month_of_birth"), col("day_of_birth"))
+    return df
+
+
+def get_nested_output(df):
+    df = df \
+        .withColumn("count", lit(df.count()).cast("long"))
+    df = df.select(
+            col("count"),
+            struct(
+                col("sequence"), col("start_date"), col("identifier"), col("names"), col("entitlements"),
+                col("year_of_birth"), col("month_of_birth"), col("day_of_birth")
+            ).alias("records")
+        ).groupBy(col("count"))\
+        .agg(
+            collect_list("records").alias("records")
+        )
     return df
 
 
 def main():
     df = get_nested_columns(file)
     df = process_data(df)
-    output_location = config.get("paths", 'output')
-    df.coalesce(1).write.json(output_location)
     df.printSchema()
-    # file.show()
+    output_location = config.get("paths", 'output')
+    # df.coalesce(1).write.json("/Users/macbookpro/IdeaProjects/pyspark_glue_jobs/src/main/resources/dataset/output/text")
+    df = get_nested_output(df)
+    df.printSchema()
+    df.coalesce(1).write.json("/Users/macbookpro/IdeaProjects/pyspark_glue_jobs/src/main/resources/dataset/output/text")
+    # file.show(10)
 
 
 if __name__ == '__main__':
